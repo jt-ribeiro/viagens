fce4d7971d992d356b47135cb5596bd61fe16d0f
jt-ribeiro <jt.ribeiro14@gmai.com>
2025-05-06 15:30:32 +0100

    Ui da pagina principal

--- app/src/main/java/com/example/viagens/MainActivity.kt
+++ app/src/main/java/com/example/viagens/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.example.viagens
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val startButton = findViewById<Button>(R.id.start_button)
+
+        startButton.setOnClickListener {
+            // Exemplo: navegar para Login
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
